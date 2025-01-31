cbb41482639f2f4adfbb5a9ef9d78a693abda186
garretfernandez <garret.fernandez.btech2022@sitpune.edu.in>
2025-01-31 17:05:30 +0530

    linking

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const ejsMate = require ("ejs-mate")
 const path = require ("path")
 const app = express()
 app.engine("ejs", ejsMate);
+app.use(express.static("css"));
+app.use(express.static(path.join(__dirname, 'hostit-html')));
+
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
@@ -13,6 +17,20 @@ app.get('/',(req, res)=>{
 
     res.render("coordinatordash.ejs");
 })
+app.get("/addstudent", (req, res) => {
+    res.render("addstudent"); // Ensure "addstudent.ejs" is in your views folder
+  });
+  app.get("/addfaculty", (req, res) => {
+    res.render("addfaculty"); // Ensure "addstudent.ejs" is in your views folder
+  });
+  app.get("/mapping", (req, res) => {
+    res.render("mapping"); // Ensure "addstudent.ejs" is in your views folder
+  });
+  app.get("/coordinatordash", (req, res) => {
+    res.render("coordinatordash"); // Ensure "addstudent.ejs" is in your views folder
+  });
+ 
+  
 
 app.listen(3000, () => {
     console.log("listening")
